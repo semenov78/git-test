745c7c9382672e45db1d1961c0554d2b37eace80
semenov78 <m.r.semenov@student.khai.edu>
2024-12-22 16:33:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer((req, res) => {
        res.end(); //end the response
     }else{
        res.write('<h1>Hello World!<h1>'); //write a response
-       res.write('<h2>My name Dmytro<h2>'); //write a response
+       res.write('<h2>My name Semenov<h2>'); //write a response
        res.end(); //end the response
     }
 });
